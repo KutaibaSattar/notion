29da0cc9bc78abcbc4a5479de8a6c2adce43af33
KutaibaSattar <kutaiba.sattar@gmail.com>
2023-03-15 14:17:07 +0400

    third

--- notiondatabase.js
+++ notiondatabase.js
@@ -11,22 +11,22 @@ const notion = new Client({
   //auth: "secret_4lrws2Ic8Yv1SrBOWGwicLpoGkrqrpDFrnRHmZBbpGe",
 });
 
-module.exports.getDatabase = async function () {
-  const response = await notion.databases.query({
-    database_id: "83a0f069e9ba4c7fa80db64304d851e2",
-    // database_id: process.env.NOTION_DATABASE_ID,
-    //database_id: "f990d49fb610448a8399d3c0bf7349b7",
-  });
+// module.exports.getDatabase = async function () {
+//   const response = await notion.databases.query({
+//     database_id: "83a0f069e9ba4c7fa80db64304d851e2",
+//     // database_id: process.env.NOTION_DATABASE_ID,
+//     //database_id: "f990d49fb610448a8399d3c0bf7349b7",
+//   });
 
-  const responseResults = response.results.map((page) => {
-    return {
-      id: page.id,
-      name: page.properties.Name.title[0]?.plain_text,
-    };
-  });
-  console.log(responseResults);
-  return responseResults;
-};
+//   const responseResults = response.results.map((page) => {
+//     return {
+//       id: page.id,
+//       name: page.properties.Name.title[0]?.plain_text,
+//     };
+//   });
+//   console.log(responseResults);
+//   return responseResults;
+// };
 
 module.exports.newEntryToDatabase = async function (
   name,
